875e9b30c4a72e81cf101bb73ec0880cf393e3df
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-05-01 21:17:47 -0300

    Finished the cards layout

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,25 +5,27 @@ import Search from '~/components/Search';
 // import api from '~/services/api';
 
 export default function Dashboard() {
-    const [playerData, setPlayerData] = useState([]);
-    // 1,
-    // 2,
-    // 3,
-    // 4,
-    // 5,
-    // 6,
-    // 7,
-    // 8,
-    // 9,
-    // 10,
-    // 11,
-    // 12,
-    // 13,
-    // 14,
-    // 15,
-    // 16,
-    // 17,
-    const [page, setPage] = useState(1);
+    const [playerData, setPlayerData] = useState([
+        // 1,
+        // 2,
+        // 3,
+        // 4,
+        // 5,
+        // 6,
+        // 7,
+        // 8,
+        // 9,
+        // 10,
+        // 11,
+        // 12,
+        // 13,
+        // 14,
+        // 15,
+        // 16,
+        // 17,
+    ]);
+
+    // const [page, setPage] = useState(1);
 
     // async function ApiR6() {
     //     const apiTest = await api.get('/stats', {
@@ -44,33 +46,32 @@ export default function Dashboard() {
             //     offset: (page - 1) * 5,
             //     limit: 5,
             // ));
-
-            const response = playerData[Object.keys(playerData)[1]];
-
-            console.tron.log(response);
+            // const response = playerData[Object.keys(playerData)[1]];
+            // console.tron.log(response);
         }
         SearchFun();
-    }, [page]);
-
-    console.tron.log('Dash', playerData);
+    }, []);
 
     return (
         <Container isAlign={!!playerData.length}>
             {playerData.length ? (
                 <Cards>
                     {playerData.map((item) => (
-                        <Content key={item.id}>
+                        <Content
+                            key={item.id}
+                            status_ranked={item.ranked}
+                            status_competition={item.competition}
+                        >
                             <img
                                 alt="avatar"
                                 src="https://ubisoft-avatars.akamaized.net/befa1d9e-179f-4f34-a5f2-4c14848cc9f6/default_256_256.png"
                             />
-
-                            <button
-                                type="button"
-                                onClick={() => setPage(page + 1)}
-                            >
-                                Test
-                            </button>
+                            <h1>{item.name}</h1>
+                            <h2>{`Play Style is ${item.play_style}`}</h2>
+                            <h2>Available to play:</h2>
+                            <div className="ranked">RANKED</div>
+                            <div className="competition">CHAMPIONSHIP</div>
+                            <div className="times">{item.times}</div>
                         </Content>
                     ))}
                 </Cards>
