4ca555aa0b9c23975444859f553441eee45c6467
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-07-03 14:47:48 -0300

    Added socket io for the chat

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import io from '~/services/io';
+import io from 'socket.io-client';
+import { useSelector } from 'react-redux';
 import api from '~/services/api';
 
 import { Container, Content, MessageField } from './styles';
 
 export default function Chat({ match }) {
-    const [chatId, setChatId] = useState([]);
+    const [chatId, setChatId] = useState(0);
     const [allMessages, setAllMessages] = useState([]);
-    const socket = io;
+
+    const profile = useSelector((state) => state.user.profile);
 
     const { id } = match.params;
+
+    const socket = io(process.env.REACT_APP_API_URL, {
+        query: { user: profile.id },
+    });
+
+    socket.on('sendMessage', (data) => {
+        setChatId(chatId <= 0 ? data.chatId : chatId);
+        setAllMessages([...allMessages, data]);
+    });
 
     useEffect(() => {
         async function getMessages() {
@@ -21,19 +32,28 @@ export default function Chat({ match }) {
                 setAllMessages(response.data.messages);
             }
         }
+
         getMessages();
     }, [id]);
 
     async function handleSubmit({ message }) {
         if (allMessages.length <= 0) {
-            await api.post(`/chat/${id}`, { message });
+            const response = await api.post(`/chat/${id}`, { message });
+            // setChatId(_id);
+            setChatId(response.data._id);
             setAllMessages([...allMessages, { message }]);
         }
 
+        console.tron.log(chatId, message);
         await api.put(`/chat/${chatId}`, { message });
 
         setAllMessages([...allMessages, { message }]);
     }
+
+    // Message received
+    // socket.on('receivedMessage', (message) => {
+    //     setAllMessages([...allMessages, { message }]);
+    // });
 
     return (
         <Container>
--- src/services/io.js
+++ src/services/io.js
@@ -1,5 +1,1 @@
-import io from 'socket.io-client';
-
-const socketIo = io(process.env.REACT_APP_API_URL);
-
-export default socketIo;
+// export default socketIo;
