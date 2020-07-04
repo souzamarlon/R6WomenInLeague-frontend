8a8d848f0a2817d022c147569fb16774eaadce5a
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-07-04 14:12:58 -0300

    Created a chat component!

---
+++ src/components/ChatMessages/styles.js
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+
+import { darken } from 'polished';
+
+export const Container = styled.div`
+    margin: auto;
+    width: 350px;
+    height: 500px;
+    background: #fff;
+
+    form {
+        display: flex;
+        flex-direction: row;
+        /* margin-top: 20px; */
+        margin: 20px 20px;
+
+        .sendField {
+            background: rgba(255, 255, 255, 0.2);
+            border: 0.5px solid #eee;
+            /* box-shadow: 0 1px 0 rgba(0, 0, 0, 0.1); */
+            border-radius: 4px;
+            height: 45px;
+            width: 80%;
+            padding: 0 15px;
+            color: #000;
+            margin: 0 0 10px;
+            box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.2);
+            &::placeholder {
+                color: #999999;
+            }
+        }
+
+        button {
+            /* background: rgba(5, 250, 88, 0.8); */
+            border: 0;
+            height: 40px;
+            width: 40px;
+            border-radius: 25px;
+            justify-content: center;
+            align-items: center;
+            margin-left: 10px;
+            display: flex;
+            box-shadow: 5px 10px 10px 3px rgba(0, 0, 0, 0.4);
+            transition: background 0.2s;
+
+            -moz-transition: all 0.3s;
+            -webkit-transition: all 0.3s;
+            transition: all 0.3s;
+
+            &:hover {
+                background: ${darken(0.02, 'rgba(5, 250, 88, 0.3)')};
+                -moz-transform: scale(1.1);
+                -webkit-transform: scale(1.1);
+                transform: scale(1.1);
+            }
+        }
+    }
+`;
+
+export const Content = styled.div`
+    height: 340px;
+    background: rgba(0, 25, 255, 0.2);
+    overflow: auto;
+    width: 350px;
+    border-radius: 10px;
+    border: 2px;
+`;
+
+export const FriendInfo = styled.div`
+    height: 65px;
+    width: 100%;
+    /* justify-content: center; */
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background: #222;
+
+    img {
+        width: 50px;
+        height: 55px;
+        border-radius: 100%;
+        margin: 0 10px;
+    }
+    h1 {
+        margin: 0 10px;
+        font-size: 12px;
+        color: #fff;
+    }
+`;
+
+export const MessageField = styled.div`
+    text-align: ${(props) => (props.textAlign ? 'left' : 'right')};
+    background: ${(props) => (props.textAlign ? '#FFF' : '#264')};
+    border-radius: 20px;
+
+    margin: 7px 10px;
+    padding: 0 10px;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+
+    .text {
+        color: #000;
+        font-size: 12px;
+        padding-top: 7px;
+    }
+`;
--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,121 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Input } from '@rocketseat/unform';
-import io from 'socket.io-client';
-import { useSelector } from 'react-redux';
-import SendIcon from '@material-ui/icons/Send';
-import { green } from '@material-ui/core/colors';
-import api from '~/services/api';
 
-import { Container, Content, FriendInfo, MessageField } from './styles';
+// import api from '~/services/api';
+
+import ChatMessages from '~/components/ChatMessages';
+
+import { Container } from './styles';
 
 export default function Chat({ match }) {
-    const [chatId, setChatId] = useState(0);
-    const [allMessages, setAllMessages] = useState([]);
-    const [friendInfo, setFriendInfo] = useState({});
-    const [avatar, setAvatar] = useState({});
-
-    const profile = useSelector((state) => state.user.profile);
-
+    const [friendId, setFriendId] = useState();
     const { id } = match.params;
 
-    const socket = io(process.env.REACT_APP_API_URL, {
-        query: { user: profile.id },
-    });
-
-    socket.on('sendMessage', (data) => {
-        setChatId(chatId <= 0 ? data.chatId : chatId);
-        setAllMessages([...allMessages, data]);
-    });
-
     useEffect(() => {
-        async function getMessages() {
-            const response = await api.get(`/chat/${id}`);
-
-            if (response.data) {
-                setChatId(response.data.messagesReceived._id);
-                setAllMessages(response.data.messagesReceived.messages);
-                setFriendInfo(response.data.userInfo);
-            }
+        async function friendIdFunction() {
+            setFriendId(id);
         }
-
-        getMessages();
+        friendIdFunction();
     }, [id]);
-
-    useEffect(() => {
-        async function getPlayerData(array) {
-            if (friendInfo.uplay) {
-                const { data } = await api.get('/stats', {
-                    params: {
-                        username: friendInfo.uplay,
-                        platform: 'pc',
-                        type: 'seasonal',
-                    },
-                });
-
-                setAvatar({
-                    avatar_url: data.avatar_url_256,
-                });
-            }
-        }
-
-        getPlayerData();
-    }, [friendInfo]);
-
-    async function handleSubmit({ message }) {
-        if (allMessages.length <= 0) {
-            const response = await api.post(`/chat/${id}`, { message });
-            // setChatId(_id);
-            setChatId(response.data._id);
-            setAllMessages([...allMessages, { message }]);
-        }
-
-        await api.put(`/chat/${chatId}`, { message });
-
-        setAllMessages([...allMessages, { message }]);
-    }
 
     return (
         <Container>
-            <FriendInfo>
-                <img
-                    alt="avatar"
-                    // src="https://ubisoft-avatars.akamaized.net/befa1d9e-179f-4f34-a5f2-4c14848cc9f6/default_256_256.png"
-                    src={avatar.avatar_url}
-                />
-                <h1>{friendInfo.name}</h1>
-            </FriendInfo>
-            <Content>
-                {allMessages.map((item) => (
-                    <>
-                        {allMessages ? (
-                            <MessageField textAlign={item.user == id}>
-                                <h2 className="text">{item.message}</h2>
-                            </MessageField>
-                        ) : null}
-                    </>
-                ))}
-            </Content>
-            <Form onSubmit={handleSubmit}>
-                <Input
-                    multiline
-                    rows={2}
-                    type="text"
-                    name="message"
-                    placeholder="Hi!"
-                    className="sendField"
-                    // style={{ width: 200 }}
-                    maxLength="140"
-                />
-                <button className="send" type="submit">
-                    <SendIcon
-                        style={{
-                            fontSize: 30,
-                            color: green[600],
-                            paddingLeft: 5,
-                        }}
-                    />
-                </button>
-            </Form>
+            {friendId ? <ChatMessages friendId={friendId} /> : null}
         </Container>
     );
 }
