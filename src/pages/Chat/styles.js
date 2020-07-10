4c3f4915452534f3ea11f664fe7f2f96cea7e871
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-07-10 15:49:26 -0300

    Aligned the friend list to center

--- src/pages/Chat/styles.js
+++ src/pages/Chat/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
     margin: auto;
     flex-direction: row;
+    justify-content: center;
     display: flex;
     width: 800px;
 `;
